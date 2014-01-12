5d2ca46437a0b6dc7e7a9ecfb14271c9333bd987
Łukasz Patalas <luke.patalas@gmail.com>
2014-01-12 19:43:06 +0100

    Extract code into separate functions

--- Format-Matches.psm1
+++ Format-Matches.psm1
@@ -1,60 +1,64 @@
 function Format-Matches {
+
     begin {
         $currentPath = ""
-        $lastLine = 0
-    }
-
-    process {
-        $match = $_
-
-        $lineNumber = $match.LineNumber - $match.Context.DisplayPreContext.Count
-
-        if ($match.Path -ne $currentPath) {
-            Write-Host
-            Write-Host $match.Path -ForegroundColor Yellow
-            $currentPath = $match.Path
-            $lastLine = 0
-        }
-        elseif ($lastLine -lt $lineNumber - 1) {
-            Write-Host "..."
-        }
-
-        if ($match.Context.DisplayPreContext.Length -gt 0) {
-            foreach ($ctxLine in $match.Context.DisplayPreContext) {
-                Write-Host "$($lineNumber): $ctxLine" -ForegroundColor DarkGray
-                $lineNumber += 1
-            }
-        }
-        
-
-        Write-Host "$($match.LineNumber): " -NoNewLine -ForegroundColor Blue
-
-        $line = $match.Line
-        $start = 0
-
-        foreach ($lineMatch in $match.Matches) {
-            if ($lineMatch.Index -gt $start) {
-                Write-Host -NoNewLine "$($line.Substring($start, $lineMatch.Index))"
-            }
-
-            Write-Host $lineMatch.Value -NoNewLine -BackgroundColor DarkBlue
-            $start = $lineMatch.Index + $lineMatch.Length
-        }
-
-        Write-Host $line.Substring($start)
-        
-        if ($match.Context.DisplayPostContext.Length -gt 0) {
-            foreach ($ctxLine in $match.Context.DisplayPostContext) {
-                $lineNumber += 1
-                Write-Host "$($lineNumber): $ctxLine" -ForegroundColor DarkGray
-            }    
-        }
-
-        $lastLine = $lineNumber
+        $lastPrintedLineNumber = 0
     }
 
     end {
         Write-Host
+    }
+
+    process {
+        function Write-FileHeader($filePath) {
+            Write-Host
+            Write-Host $filePath -ForegroundColor Yellow
+        }
+
+        function Write-Context($context, $lineNumber) {
+            if ($context.Length -gt 0) {
+                foreach ($line in $context) {
+                    Write-Host "$($lineNumber): $line" -ForegroundColor DarkGray
+                    $lineNumber++
+                }
+            }
+        }
+
+        function Write-Match($match) {
+            Write-Host "$($match.LineNumber): " -NoNewLine -ForegroundColor Blue
+
+            $line = $match.Line
+            $start = 0
+
+            foreach ($lineMatch in $match.Matches) {
+                if ($lineMatch.Index -gt $start) {
+                    Write-Host -NoNewLine "$($line.Substring($start, $lineMatch.Index))"
+                }
+
+                Write-Host $lineMatch.Value -NoNewLine -BackgroundColor DarkBlue
+                $start = $lineMatch.Index + $lineMatch.Length
+            }
+
+            Write-Host $line.Substring($start)
+        }
+
+        $match = $_
+        $firstVisibleLineNumber = $match.LineNumber - $match.Context.DisplayPreContext.Length
+
+        if ($match.Path -ne $currentPath) {
+            Write-FileHeader $match.Path
+            $currentPath = $match.Path
+            $lastPrintedLineNumber = 0
+        }
+        elseif ($lastPrintedLineNumber -lt $firstVisibleLineNumber - 1) {
+            Write-Host "..." -ForegroundColor DarkGray
+        }
+
+        Write-Context $match.Context.DisplayPreContext ($match.LineNumber - $match.Context.DisplayPreContext.Count)
+        Write-Match $match
+        Write-Context $match.Context.DisplayPostContext ($match.LineNumber + 1)
+
+        $lastPrintedLineNumber = $match.LineNumber + $match.Context.DisplayPostContext.Length
     }
 }
 
